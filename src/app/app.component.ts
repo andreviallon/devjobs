b376caf5884a59cd9f0db2f51476a9826e4f7159
Andre Viallon <viallon.andre@gmail.com>
2021-03-20 14:53:14 +0100

    WIP: header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+    <header></header>
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'github-jobs';
+
 }
---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'header',
+  template: `
+    <div class="header">
+      <h1>Devjobs</h1>
+    </div>
+  `,
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent {
+// <img src="../../../assets/header-background.svg" alt="" />
+}
