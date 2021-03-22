219157c4a7013767f58ab29ca6eb3cf12773f429
Andre Viallon <viallon.andre@gmail.com>
2021-03-22 16:48:26 +0100

    WIP: job card styling

--- src/app/components/job-card/job-card.component.ts
+++ src/app/components/job-card/job-card.component.ts
@@ -5,8 +5,14 @@ import { Job } from 'src/app/states/jobsState/jobState.state';
   selector: 'job-card',
   template: `
     <div class="card">
+      <img src="{{ job.company_logo }}" />
+      <div class="flex-container">
+        <p>{{ job.type }}</p>
+        <div class="dot"></div>
+      </div>
       <h3>{{ job.title }}</h3>
       <p>{{ job.company }}</p>
+      <h4 class="capitalize location">{{ job.location }}</h4>
     </div>
   `,
   styleUrls: ['./job-card.component.scss']
