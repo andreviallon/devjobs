18df41c4a28c82a6c0898af6a4dc7810ddf3c4d3
Andre Viallon <viallon.andre@gmail.com>
2021-08-10 18:29:08 +0200

    change title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,14 +7,13 @@ import { Navigate } from '@ngxs/router-plugin';
   template: `
     <div class="header">
       <div class="flex-container">
-        <h1 (click)="navigateHome()">devjobs</h1>
+        <h1 (click)="navigateHome()">Devjobs</h1>
         <dark-mode-toggle></dark-mode-toggle>
       </div>
     </div>
   `,
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
   @Dispatch() navigateHome = () => new Navigate(['/']);
-
 }
